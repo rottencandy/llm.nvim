f442e55513a64439c374f42a5f552d92684d36b5
saud <md.saud020@gmail.com>
2025-02-21 00:15:21 +0530

    use temporary buffer for storing suggestions

--- lua/llm/init.lua
+++ lua/llm/init.lua
@@ -1,5 +1,8 @@
 local http = require("plenary.curl").request
 local api = vim.api
+-- weird nvim lua deprecation issue
+-- https://github.com/hrsh7th/nvim-cmp/issues/1017
+local unpack = unpack or table.unpack
 
 local CF_MODEL_ID = "@hf/thebloke/deepseek-coder-6.7b-base-awq"
 local API_URL = "https://api.cloudflare.com/client/v4/accounts/" .. CF_ACC_ID .. "/ai/run/" .. CF_MODEL_ID
@@ -63,11 +66,85 @@ local split = function(str)
     return out
 end
 
+---strip trailing newlines from string
+---@param str string
+---@return string
+local strip = function(str)
+    return str:match("^(.*[^%s])")
+end
+
+---apply lines to buffer
+---@param buf integer
+---@param line_pos integer
+---@param lines string[]
+local apply_lines = function(buf, line_pos, lines)
+    -- set first line separately,
+    -- because nvim_buf_set_lines does not allow appending to existing lines
+    api.nvim_buf_set_text(buf, line_pos, -1, line_pos, -1, { lines[1] })
+    if #lines > 1 then
+        api.nvim_buf_set_lines(buf, line_pos, line_pos, false, { unpack(lines, 2) })
+    end
+end
+
+---add virtual text to current buffer and allow applying it using a confirmation prompt
+---@param line_pos integer line to add text to, 0-indexed. first line will be appended to any existing text on line
+---@param text string string of text to apply, will be split by newlines and applied to multiple lines
+local add_virtual_text = function(line_pos, text)
+    local lines = split(text)
+    -- holds 2nd to last lines
+    -- first line will be passed as `virt_text`
+    local virt_lines = {}
+    if #lines > 1 then
+        for i = 2, #lines do
+            virt_lines[i - 1] = { { lines[i], "Comment" } }
+        end
+    end
+    api.nvim_buf_clear_namespace(api.nvim_get_current_buf(), ns_id, line_pos, line_pos)
+    local extmark_id = api.nvim_buf_set_extmark(api.nvim_get_current_buf(), ns_id, line_pos, -1, {
+        virt_text_pos = "inline",
+        virt_text = {
+            -- use the "Comment" highlight group for now
+            { lines[1], "Comment" },
+        },
+        virt_lines = virt_lines,
+    })
+
+    vim.ui.input(
+        { prompt = "Accept? (y/n): " },
+        ---@param input string
+        function(input)
+            if input == "y" or input == "Y" then
+                apply_lines(api.nvim_get_current_buf(), line_pos, lines)
+            end
+        end
+    )
+    api.nvim_buf_del_extmark(api.nvim_get_current_buf(), ns_id, extmark_id)
+end
+
+---prepare temporary buffer to display suggestion
+---@param contents string
+---@param target_pos integer
+---@param target_buf integer
+local prepare_temp_buf = function(contents, target_pos, target_buf)
+    local buf = api.nvim_create_buf(false, true)
+    local lines = split(strip(contents))
+    api.nvim_buf_set_name(buf, "llm://test")
+    api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    api.nvim_set_option_value("filetype", "txt", { buf = buf })
+    vim.cmd("vsplit")
+    local win = vim.api.nvim_get_current_win()
+    api.nvim_win_set_buf(win, buf)
+
+    api.nvim_buf_create_user_command(buf, "LLMApply", function()
+        apply_lines(target_buf, target_pos, lines)
+    end, { nargs = 0 })
+end
+
 ---@param line_start integer
 ---@param line_end integer
 function M.suggest(line_start, line_end)
     -- line indexing needs to be 0 based
-    local prompt = api.nvim_buf_get_lines(0, line_start - 1, line_end, false)
+    local prompt = api.nvim_buf_get_lines(api.nvim_get_current_buf(), line_start - 1, line_end, false)
     local suggestion, err = get_completion(table.concat(prompt, "\n"), vim.bo.filetype)
     if err then
         vim.notify("Error: " .. (err or "<nil>"), vim.log.levels.ERROR)
@@ -79,37 +156,7 @@ function M.suggest(line_start, line_end)
     end
 
     local line_pos = line_end - 1
-    local suggestion_lines = split(suggestion)
-    -- holds 2nd to last lines
-    -- first line will be passed as `virt_text`
-    local virt_lines = {}
-    if #suggestion_lines > 1 then
-        for i = 2, #suggestion_lines do
-            virt_lines[i - 1] = { { suggestion_lines[i], "Comment" } }
-        end
-    end
-    api.nvim_buf_clear_namespace(0, ns_id, line_pos, line_pos)
-    local extmark_id = api.nvim_buf_set_extmark(0, ns_id, line_pos, -1, {
-        virt_text_pos = "inline",
-        virt_text = {
-            -- use the "Comment" highlight group for now
-            { suggestion_lines[1], "Comment" },
-        },
-        virt_lines = virt_lines,
-    })
-    vim.ui.input(
-        { prompt = "Accept? (y/n): " },
-        ---@param input string
-        function(input)
-            if input == "y" or input == "Y" then
-                -- set first line separately,
-                -- because nvim_buf_set_lines does not allow appending to existing lines
-                api.nvim_buf_set_text(0, line_pos, -1, line_pos, -1, { suggestion_lines[1] })
-                api.nvim_buf_set_lines(0, line_pos, line_pos, false, { table.unpack(suggestion_lines, 2) })
-            end
-        end
-    )
-    api.nvim_buf_del_extmark(0, ns_id, extmark_id)
+    prepare_temp_buf(suggestion, line_pos, api.nvim_get_current_buf())
 end
 
 function M.setup()
